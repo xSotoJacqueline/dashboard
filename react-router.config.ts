075ac40b0b61af35e56fe9c30f5bf9bac576d83b
FilyBox <meno117@outlook.com>
2025-07-24 12:53:22 -0600

    feat: vercel configuration for deployment

--- react-router.config.ts
+++ react-router.config.ts
@@ -1,6 +1,8 @@
 import type { Config } from "@react-router/dev/config";
+import { vercelPreset } from '@vercel/react-router/vite';
 
 export default {
   appDirectory: "src",
   ssr: true,
+  presets: [vercelPreset()],
 } satisfies Config;
