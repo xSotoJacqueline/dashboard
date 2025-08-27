5db812719627e24bee60b1879d611aaf9fb0ca19
FilyBox <meno117@outlook.com>
2025-08-27 17:40:50 -0600

    style: changed component in player tab

--- src/components/tabs/players-tab.tsx
+++ src/components/tabs/players-tab.tsx
@@ -1,13 +1,3 @@
-import { motion, MotionConfig } from 'framer-motion'
-import NumberFlow, { useCanAnimate } from '@number-flow/react'
-import { cn } from "@/lib/utils"
-import { TrendingUp } from 'lucide-react'
-import {
-  Card,
-  CardTitle,
-  CardHeader,
-  CardContent,
-} from "@/components/ui/card"
 import { Progress } from '../ui/progress';
 import { BarChartPerDayMarketing } from '../marketing/barChart-campaigns-perday';
 import UsersByCity from '../marketing/users-by-city'
@@ -16,12 +6,8 @@ import { useQueries } from '@tanstack/react-query'
 import { TopCard, TopCardContent, TopCardFooter, TopCardHeader, TopCardTitle, TopCardValue } from "../ui/general-top-card";
 import { BarChartRegistersPerDayMarketing } from '../marketing/barChart-registers-perday'
 
-const MotionNumberFlow = motion.create(NumberFlow)
-const MotionArrowUp = motion.create(TrendingUp)
 
 export default function PlayersTab({queryString,labelTimePeriod}: {queryString?: string, labelTimePeriod?: string}) {
-    const canAnimate = useCanAnimate()
-
     const [averageTimeOnPage ] = useQueries({
       queries: [getAverageTimeOnPage({queryString})],
     });
@@ -33,27 +19,25 @@ export default function PlayersTab({queryString,labelTimePeriod}: {queryString?:
       <BarChartRegistersPerDayMarketing queryString={queryString} />
     </div>
     <div className='h-full md:h-[35cqh] w-full grid grid-cols-2 md:grid-cols-6 gap-6'>
-      <Card className="w-full h-full border-0 gap-0 col-span-2 md:col-span-3 lg:col-span-2 space-y-0">
-          <CardHeader>
-              <CardTitle className="text-xl font-semibold">Retención 7 días</CardTitle>
-          </CardHeader>
-          <CardContent className=" @container-normal relative sm:pt-0 h-full" >
-            <div className="flex flex-col gap-4 justify-between h-full w-full">
-                <div className="h-fit w-full flex gap-4">
-                  <NumberFlow
-                    value={0.078}
-                    format={{ style: 'percent',  maximumFractionDigits: 2  }}
-                    className="text-4xl font-bold"
-                  />
-                </div>
-                <div className="flex xl:flex-row  lg:items-start lg:flex-col items-center justify-between w-full">
-                  <Progress value={70} className='' />
-                </div>
-            </div>
-              
-          </CardContent>
-      </Card>
-
+      <TopCard
+        isLoading={averageTimeOnPage.isPending}
+        isError={averageTimeOnPage.isError}
+        iconSize={24}
+        iconStrokeWidth={2}
+        refetch={averageTimeOnPage.refetch}
+        index={21}
+        valueFormat="currency"
+        containerClassName='w-full h-full border-0 gap-0 col-span-2 md:col-span-3 lg:col-span-2 space-y-0'
+        className="flex flex-col justify-between font-normal gap-3"
+      >
+        <TopCardHeader className="flex ">
+          <TopCardTitle className="">Retención 7 días</TopCardTitle>
+        </TopCardHeader>
+        <TopCardContent className='gap-4 flex flex-col justify-start pb-7'>
+          <TopCardValue valueFormat="percent" value={20}   />
+          <Progress value={70} className='' />
+        </TopCardContent>
+      </TopCard>
 
       <TopCard
         isLoading={averageTimeOnPage.isPending}
@@ -67,7 +51,7 @@ export default function PlayersTab({queryString,labelTimePeriod}: {queryString?:
         className="flex flex-col justify-between font-normal gap-3"
       >
         <TopCardHeader className="flex ">
-          <TopCardTitle className="min-h-14">Sesión promedio</TopCardTitle>
+          <TopCardTitle className="">Sesión promedio</TopCardTitle>
         </TopCardHeader>
         <TopCardContent className='gap-4 flex-row justify-start'>
           <TopCardValue suffix="m" valueFormat="decimal" value={averageTimeOnPage.data?.minutes ? averageTimeOnPage.data.minutes : 0}   />
@@ -77,64 +61,27 @@ export default function PlayersTab({queryString,labelTimePeriod}: {queryString?:
       </TopCard>
 
 
-      <Card className="w-full h-full border-0 gap-0 col-span-2 md:col-span-6 lg:col-span-2 space-y-0">
-          <CardHeader>
-              <CardTitle className="text-xl font-semibold">Sesión promedio</CardTitle>
-          </CardHeader>
-          <CardContent className=" @container-normal relative sm:pt-0 h-full" >
-            <div className="flex flex-col gap-4 justify-between h-full w-full">
-                <div className="h-fit w-full flex gap-4">
-                  <NumberFlow
-                    value={0.078}
-                    format={{ style: 'percent',  maximumFractionDigits: 2  }}
-                    className="text-4xl font-bold"
-                  />
-                </div>
-                <div className="flex xl:flex-row  lg:items-start items-center justify-between w-full">
-                  <span className="text-sm text-gray-600">Ultimos 28 días</span>
-                    <MotionConfig
-                      // Disable layout animations if NumberFlow can't animate.
-                      // This worked better than setting layout={canAnimate}
-                      transition={{
-                        layout: canAnimate ? { duration: 0.9, bounce: 0, type: 'spring' } : { duration: 0 }
-                      }}
-                    >
-                      <motion.span
-                        className={cn(
-                          2 > 0 ? 'bg-green-foliatti' : 'bg-red-500',
-                          'inline-flex gap-1 items-center px-[0.3em] text-lg text-white transition-colors duration-300'
-                        )}
-                        layout
-                        style={{ borderRadius: 999 }}
-                      >
-                        <MotionArrowUp
-                          className="mr-0.5 size-[0.70em]"
-                          absoluteStrokeWidth
-                          strokeWidth={3}
-                          layout // undo parent
-                          transition={{
-                            rotate: canAnimate ? { type: 'spring', duration: 0.5, bounce: 0 } : { duration: 0 }
-                          }}
-                          animate={{ rotate: 2 > 0 ? 0 : -180 }}
-                          initial={false}
-                        />
-                        <MotionNumberFlow
-                          value={2}
-                          className="font-medium text-sm"
-                          format={{ style: 'percent', maximumFractionDigits: 2, signDisplay: 'always' }}
-                          style={{ ['--number-flow-char-height' as string]: '0.85em', ['--number-flow-mask-height' as string]: '0.3em' }}
-                          layout
-                          layoutRoot
-                        />
-                      </motion.span>
-                    </MotionConfig>
-                </div>
-            </div>
-              
-          </CardContent>
-      </Card>
-    </div>
+      <TopCard
+        isLoading={averageTimeOnPage.isPending}
+        isError={averageTimeOnPage.isError}
+        iconSize={24}
+        iconStrokeWidth={2}
+        refetch={averageTimeOnPage.refetch}
+        index={23}
+        valueFormat="currency"
+        containerClassName='w-full h-full border-0 gap-0 col-span-2 md:col-span-3 lg:col-span-2 space-y-0'
+        className="flex flex-col justify-between font-normal gap-3"
+      >
+        <TopCardHeader className="flex ">
+          <TopCardTitle className="">Tasa de conversión</TopCardTitle>
+        </TopCardHeader>
+        <TopCardContent className='gap-4 flex-row justify-start'>
+          <TopCardValue valueFormat="percent" value={10}   />
+        </TopCardContent>
+        <TopCardFooter percentageValue={32} label={labelTimePeriod ? labelTimePeriod : `Últimos 28 días`} showPercentage={true}  />
+      </TopCard>
 
+    </div>
     <UsersByCity />
 
 
